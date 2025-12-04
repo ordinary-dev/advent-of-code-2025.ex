376f3ddafb6908a1bf7550c0927476a773fa071d
Ivan Reshetnikov <admin@ivanreshetnikov.ru>
2025-12-04 13:34:39 +0500

    Simplify test cases

--- day01/test/dial_test.exs
+++ day01/test/dial_test.exs
@@ -2,45 +2,26 @@ defmodule DialTest do
   use ExUnit.Case
   doctest Dial
 
-  test "example 1" do
-    res =
-      Dial.get_password(
-        [
-          "L68",
-          "L30",
-          "R48",
-          "L5",
-          "R60",
-          "L55",
-          "L1",
-          "L99",
-          "R14",
-          "L82"
-        ],
-        &Dial.count_zeros_after_rotation/1
-      )
+  @input [
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82"
+  ]
 
+  test "example 1" do
+    res = Dial.get_password(@input, &Dial.count_zeros_after_rotation/1)
     assert res == 3
   end
 
   test "example 2" do
-    res =
-      Dial.get_password(
-        [
-          "L68",
-          "L30",
-          "R48",
-          "L5",
-          "R60",
-          "L55",
-          "L1",
-          "L99",
-          "R14",
-          "L82"
-        ],
-        &Dial.count_all_zeros/1
-      )
-
+    res = Dial.get_password(@input, &Dial.count_all_zeros/1)
     assert res == 6
   end
 end
